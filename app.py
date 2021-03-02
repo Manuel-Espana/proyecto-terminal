bcd9203b213dc37de638cee55652ef58a02fdeba
Anselmo Calderón <anselmocalderon10@gmail.com>
2021-03-01 19:30:20 -0500

    Auto stash before merge of "develop" and "origin/develop"

--- app.py
+++ app.py
@@ -3,29 +3,33 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-
 app.config['MYSQL_HOST'] = 'localhost:3000'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_db'] = ''
+app.config['MYSQL_db'] = 'bitacora'
 
 mysql = MySQL()
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/resultados.html')
 def resultados():
     return render_template('resultados.html')
+
 
 @app.route('/visitantes.html')
 def visitantes():
     return render_template('visitantes.html')
 
+
 @app.route('/bitacora.html')
 def bitacora():
     return render_template('bitacora.html')
+
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
