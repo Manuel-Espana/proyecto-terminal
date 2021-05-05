f8957a781896cd9371277a290999cffc8615afb9
César Alfredo Sánchez Cen <160300142@ucaribe.edu.mx>
2021-05-05 18:53:03 -0500

    Models section

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, url_for, redirect, flash, request, send_file, g, session
 from flask_mysqldb import MySQL
 from os import urandom
+from statistics import mean
+from sklearn.metrics import make_scorer, explained_variance_score, mean_absolute_error, mean_squared_log_error, mean_squared_error, median_absolute_error, r2_score
+from sklearn.model_selection import GridSearchCV, TimeSeriesSplit, cross_val_score
+from sklearn.linear_model import LinearRegression
+from sklearn.neural_network import MLPRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+from statsmodels.tsa.api import VAR
 import datetime
 import locale
 import time
@@ -8,6 +17,7 @@ import pandas as pd
 import plotly
 import plotly.graph_objs as go
 import json
+import numpy as np
 
 date = datetime.date.today()
 
@@ -19,7 +29,7 @@ app.secret_key = urandom(24)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '123456789'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'bitacora'
 mysql = MySQL(app)
 
@@ -143,6 +153,18 @@ def resultados():
     fecha = graf_fecha()
     dia = graf_dia()
     return render_template('/resultados.html', plot1 = he, plot2 = hs, plot3 = usuario, plot4 = dep, plot5 = fecha, plot6 = dia)
+
+#Despliegue de los dashboards en la sección de resultados
+@app.route('/modelos.html')
+def modelos():
+    if not g.user:
+        return redirect(url_for('login'))
+    if g.user[0][3] != 2:
+        return redirect(url_for('index'))
+    dia_sig = graf_modelo_diaSig()
+    mantenimiento = graf_modelo_mantenimiento()
+    colaborador = graf_modelo_colaborador()
+    return render_template('/modelos.html', plot1 = dia_sig, plot2 = mantenimiento, plot3 = colaborador)
 
 #Funciones para la creacion de las graficas de Resultados
 def graf_he():
@@ -283,6 +305,198 @@ def graf_dia():
     graphJSON = json.dumps(data, cls = plotly.utils.PlotlyJSONEncoder)
 
     return graphJSON
+
+def graf_modelo_diaSig():
+    #Se crea la coneccion con la BD y se hace la consulta
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT he,hs FROM registro')
+    data = cur.fetchall()
+    cur.close()
+    #La consulta se convierte a un DataFrame para su manipulacion
+    df = pd.DataFrame(list(data), columns = ['he','hs'])
+    df['he'] = pd.to_datetime(df['he'])
+    df['hs'] = pd.to_datetime(df['hs'])
+    df['h_diff'] = (df.hs - df.he).dt.total_seconds() / 3600.0
+    df['he'] = df['he'].dt.floor('H').dt.time
+    df['hs'] = df['hs'].dt.floor('H').dt.time
+    dia_sig = df[['h_diff']]
+    dia_sig['h_yesterday'] = dia_sig.h_diff.shift()
+    dia_sig['h_yesterday_diff'] = dia_sig.h_yesterday.diff()
+    dia_sig['h_yesterday-1'] = dia_sig.h_yesterday.shift()
+    dia_sig['h_yesterday-1_diff'] = dia_sig['h_yesterday-1'].diff()
+    dia_sig = dia_sig.dropna()
+    dia_sig = dia_sig.reset_index(drop=True)
+    
+    X_train, X_test, y_train, y_test = Preparacion(dia_sig)
+    rmse_score = make_scorer(rmse, greater_is_better = False)
+    model = LinearRegression()
+    param_search = {
+        'fit_intercept':[True,False], 
+        'normalize':[True,False], 
+        'copy_X':[True, False]
+    }
+
+    tscv = TimeSeriesSplit(n_splits = 10)
+    gsearch = GridSearchCV(estimator=model, cv=tscv, param_grid=param_search, scoring = rmse_score)
+    gsearch.fit(X_train, y_train)
+    best_model = gsearch.best_estimator_
+
+    y_true = y_test.values
+    y_pred = best_model.predict(X_test)
+
+    linea1 = go.Scatter(
+        x = list(range(len(y_true))),
+        y = y_test,
+        name = 'Valores reales'
+    )
+
+    linea2 = go.Scatter(
+        x = list(range(len(y_pred))),
+        y = y_pred,
+        name = 'Valores predichos'
+    )
+    data = go.Figure([linea1,linea2])
+    data.update_layout(title = '<b>Predicción de tiempo de estadía para el siguiente día</b>', xaxis_title = 'Número de predicción', yaxis_title = 'Tiempo de estadía en horas', title_font_size = 15)
+    graphJSON = json.dumps(data, cls = plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
+
+def graf_modelo_mantenimiento():
+    #Se crea la coneccion con la BD y se hace la consulta
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT he,hs,departamento FROM registro')
+    data = cur.fetchall()
+    cur.close()
+    #La consulta se convierte a un DataFrame para su manipulacion
+    df = pd.DataFrame(list(data), columns = ['he','hs','departamento'])
+    df['he'] = pd.to_datetime(df['he'])
+    df['hs'] = pd.to_datetime(df['hs'])
+    df['h_diff'] = (df.hs - df.he).dt.total_seconds() / 3600.0
+    df['he'] = df['he'].dt.floor('H').dt.time
+    df['hs'] = df['hs'].dt.floor('H').dt.time
+    rh = df[df['departamento'] == 'Mantenimiento']
+    rh = rh[['departamento','h_diff']]
+    rh['h_yesterday'] = rh.h_diff.shift()
+    rh['h_yesterday_diff'] = rh.h_yesterday.diff()
+    rh['h_yesterday-1'] = rh.h_yesterday.shift()
+    rh['h_yesterday-1_diff'] = rh['h_yesterday-1'].diff()
+    rh = rh.dropna()
+    rh = rh.reset_index(drop=True)
+
+    X_train, X_test, y_train, y_test = Preparacion(rh)
+    rmse_score = make_scorer(rmse, greater_is_better = False)
+    model = LinearRegression()
+    param_search = {
+        'fit_intercept':[True,False], 
+        'normalize':[True,False], 
+        'copy_X':[True, False]
+    }
+
+    tscv = TimeSeriesSplit(n_splits = 10)
+    gsearch = GridSearchCV(estimator=model, cv=tscv, param_grid=param_search, scoring = rmse_score)
+    gsearch.fit(X_train, y_train)
+    best_model = gsearch.best_estimator_
+
+    y_true = y_test.values
+    y_pred = best_model.predict(X_test)
+
+    linea1 = go.Scatter(
+        x = list(range(len(y_true))),
+        y = y_test,
+        name = 'Valores reales'
+    )
+
+    linea2 = go.Scatter(
+        x = list(range(len(y_pred))),
+        y = y_pred,
+        name = 'Valores predichos'
+    )
+    data = go.Figure([linea1,linea2])
+    data.update_layout(title = '<b>Predicción de tiempo de estadía para el departamento de Mantenimiento</b>', xaxis_title = 'Número de predicción', yaxis_title = 'Tiempo de estadía en horas', title_font_size = 15)
+    graphJSON = json.dumps(data, cls = plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
+
+def graf_modelo_colaborador():
+    #Se crea la coneccion con la BD y se hace la consulta
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT he,hs,motivo_ingreso FROM registro')
+    data = cur.fetchall()
+    cur.close()
+    #La consulta se convierte a un DataFrame para su manipulacion
+    df = pd.DataFrame(list(data), columns = ['he','hs','motivo_ingreso'])
+    df['he'] = pd.to_datetime(df['he'])
+    df['hs'] = pd.to_datetime(df['hs'])
+    df['h_diff'] = (df.hs - df.he).dt.total_seconds() / 3600.0
+    df['he'] = df['he'].dt.floor('H').dt.time
+    df['hs'] = df['hs'].dt.floor('H').dt.time
+    colaborador = df[df['motivo_ingreso'] == 'Colaborador']
+    colaborador = colaborador[['motivo_ingreso','h_diff']]
+    colaborador['h_yesterday'] = colaborador.h_diff.shift()
+    colaborador['h_yesterday_diff'] = colaborador.h_yesterday.diff()
+    colaborador['h_yesterday-1'] = colaborador.h_yesterday.shift()
+    colaborador['h_yesterday-1_diff'] = colaborador['h_yesterday-1'].diff()
+    colaborador = colaborador.dropna()
+    colaborador = colaborador.reset_index(drop=True)
+
+    X_train, X_test, y_train, y_test = Preparacion(colaborador)
+    rmse_score = make_scorer(rmse, greater_is_better = False)
+    model = LinearRegression()
+    param_search = {
+        'fit_intercept':[True,False], 
+        'normalize':[True,False], 
+        'copy_X':[True, False]
+    }
+
+    tscv = TimeSeriesSplit(n_splits = 10)
+    gsearch = GridSearchCV(estimator=model, cv=tscv, param_grid=param_search, scoring = rmse_score)
+    gsearch.fit(X_train, y_train)
+    best_model = gsearch.best_estimator_
+
+    y_true = y_test.values
+    y_pred = best_model.predict(X_test)
+
+    linea1 = go.Scatter(
+        x = list(range(len(y_true))),
+        y = y_test,
+        name = 'Valores reales'
+    )
+
+    linea2 = go.Scatter(
+        x = list(range(len(y_pred))),
+        y = y_pred,
+        name = 'Valores predichos'
+    )
+    data = go.Figure([linea1,linea2])
+    data.update_layout(title = '<b>Predicción de tiempo de estadía para Colaboradores</b>', xaxis_title = 'Número de predicción', yaxis_title = 'Tiempo de estadía en horas', title_font_size = 15)
+    graphJSON = json.dumps(data, cls = plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
+
+def Resultados_Regresion(y_true, y_pred):
+    #Métricas de regresión
+    mae = mean_absolute_error(y_true, y_pred) 
+    mse = mean_squared_error(y_true, y_pred) 
+    r2 = r2_score(y_true, y_pred)
+    print('R2: ', round(r2,4))
+    print('MAE: ', round(mae,4))
+    print('RMSE: ', round(np.sqrt(mse),4))
+    print('Promedio valor real', round(mean(y_true), 3))
+    print('Promedio valor predicho', round(mean(y_pred), 3))
+
+def rmse(actual, predict):
+    predict = np.array(predict)
+    actual = np.array(actual)
+    distance = predict - actual
+    square_distance = distance ** 2
+    mean_square_distance = square_distance.mean()
+    score = np.sqrt(mean_square_distance)
+    return score
+
+def Preparacion(df):
+    X = df[['h_yesterday','h_yesterday_diff', 'h_yesterday-1','h_yesterday-1_diff']]
+    y = df.h_diff
+    train_size = int(len(df) * 0.80)
+    X_train, X_test = X[0:train_size], X[train_size:len(X)]
+    y_train, y_test = y[0:train_size], y[train_size:len(X)]
+    return X_train, X_test, y_train, y_test
 
 #Registro de visitantes en la sección de visitantes
 @app.route('/visitantes.html', methods=['POST'])
